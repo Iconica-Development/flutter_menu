e8847a9550cf70bab63838727d49239ccfbec05a
Thomas Klein Langenhorst <thomas@iconica.nl>
2022-12-13 13:22:14 +0100

    Imrpoved README, added Github Actions + depandabot, improved example.Added screenshot

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-[![style: effective dart](https://img.shields.io/badge/style-effective_dart-40c4ff.svg)](https://github.com/tenhobi/effective_dart)
+[![pub package](https://img.shields.io/pub/v/flutter_menu.svg)](https://github.com/Iconica-Development) [![Build status](https://img.shields.io/github/workflow/status/Iconica-Development/flutter_menu/CI)](https://github.com/Iconica-Development/flutter_menu/actions/new) [![style: effective dart](https://img.shields.io/badge/style-effective_dart-40c4ff.svg)](https://github.com/tenhobi/effective_dart)
+
+# Flutter Menu
 
 Flutter package for using creating a configurable app menu
+
+![Screenshot](example_screenshot.png)
+
+## Setup
+
+To use this package, add `flutter_menu` as a dependency in your pubspec.yaml file.
+
+## How to use
+
+See the [Example Code](example/lib/main.dart) for an example on how to use this package.
 
 ## Issues
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,33 +36,61 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: AppMenu(
-            actions: [
-              MenuAction.custom(
-                builder: (context) => const Text('test'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: SizedBox(
+                height: size.height - (MediaQuery.of(context).padding.top * 2),
+                width: size.width * 0.85,
+                child: AppMenu(
+                  exit: IconButton(
+                    icon: const Icon(
+                      Icons.close,
+                    ),
+                    onPressed: () {},
+                  ),
+                  logout: MenuAction.text(
+                    text: 'logout',
+                    onTap: (context) {},
+                    icon: Icons.logout,
+                    textStyle: Theme.of(context).textTheme.headline3!,
+                  ),
+                  actions: [
+                    MenuAction.custom(
+                      builder: ((_) => const CircleAvatar(
+                            radius: 50,
+                            child: Center(
+                              child: Text('JD'),
+                            ),
+                          )),
+                    ),
+                    MenuAction.text(
+                      text: 'dashboard',
+                      onTap: (context) {},
+                      icon: Icons.dashboard,
+                      textStyle: Theme.of(context).textTheme.headline3!,
+                    ),
+                    MenuAction.divider(),
+                    MenuAction.text(
+                      text: 'List',
+                      onTap: (context) {},
+                      icon: Icons.list,
+                      textStyle: Theme.of(context).textTheme.headline3!,
+                    ),
+                    MenuAction.text(
+                      text: 'settings',
+                      onTap: (context) {},
+                      textStyle: Theme.of(context).textTheme.headline3!,
+                    ),
+                  ],
+                  child: Container(),
+                ),
               ),
-              MenuAction.divider(),
-              MenuAction.search(onSearch: (context, val) {
-                debugPrint('on search with $val');
-              }),
-              MenuAction.spacer(),
-              MenuAction.text(
-                text: 'text',
-                onTap: (context) {
-                  debugPrint('on tap text action');
-                },
-              ),
-            ],
-            child: Container(),
-          ),
+            ),
+          ],
         ),
       ),
     );
